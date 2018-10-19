bedba1332c0be1b67be0115ae5838fb6cee70fd4
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-10-19 12:38:03 -0500

    Funcionalidad agrgada, paseo entre pestanas

--- DashboardSS/src/app/app.component.ts
+++ DashboardSS/src/app/app.component.ts
@@ -11,7 +11,9 @@ import * as $ from 'jquery';
 export class AppComponent {
   title = 'Titulo del Sitio';
   loadComponent = false;
-
+  anterior = 0;
+  intAnt = 0;
+  now = 1;
 
   whichTransitionEvent(){
     var t,
@@ -89,42 +91,61 @@ export class AppComponent {
     };
 
 
-    mapContent = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-    // a new Info Window is created
-    infoWindow = new google.maps.InfoWindow();
-
-    // Event that closes the Info Window with a click on the map
-    google.maps.event.addListener(mapContent, 'click', function() {
-      infoWindow.close();
-    });
-
-    if(setMap == "location") {
-      this.displayMarkers("location");
-
-    }
-    if(setMap == "nearMe") {
-      this.displayMarkers("nearMe");
-
-    }
-    if (setMap == "needsMe") {
-      this.displayMarkers("needsMe");
-    }
-
   }*/
 
-    public NextPage() {
-      //console.log("Hello");
-      var next = $("#content_page");
-      next.addClass("fadeOutLeft");
-      next.addClass("animated");
+    public NextPage(val, xant) {
+
+      if(this.now != xant)
+      {
+        if(this.anterior != 0)
+        {
+          //alert(this.anterior);
+          var before = $("#" + this.anterior);
+
+          var next = $("#"+ val);
+
+          if(xant < this.intAnt)
+          {
+            next.removeClass("fadeOutLeft");
+            next.removeClass("animated");
+
+            before.addClass("fadeOutRight");
+            before.addClass("animated");
+
+            next.addClass("fadeInLeft");
+            next.addClass("animated");
+
+            setTimeout(function() {
+              before.css("display","none");
+            },1000);
+          } else {
+              next.removeClass("fadeOutRight");
+              next.removeClass("animated");
+
+              before.addClass("fadeOutLeft");
+              before.addClass("animated");
+
+              next.css("display","block");
+              next.addClass("fadeInRight");
+              next.addClass("animated");
+          }
+        } else {
+
+          var next = $("#content_page");
+          next.addClass("fadeOutLeft");
+          next.addClass("animated");
 
 
-      next = $("#content_map");
-      next.css("display", "block");
-      next.addClass("fadeInRight");
-      next.addClass("animated");
+          next = $("#"+ val);
+          next.css("display", "block");
+          next.addClass("fadeInRight");
+          next.addClass("animated");
+        }
 
+        this.anterior = val;
+        this.intAnt = xant;
+        this.now = xant;
+      }
     }
 
 
