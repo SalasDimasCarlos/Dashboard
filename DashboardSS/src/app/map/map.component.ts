605138aa6b1932b42f5425461e2f44e12f010473
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-10-18 22:34:32 -0500

    Mapa rendereado, rutas

--- DashboardSS/src/app/map/map.component.ts
+++ DashboardSS/src/app/map/map.component.ts
@@ -8,11 +8,21 @@ import { AgmMap } from '@agm/core';
 })
 
 export class MapComponent {
+  directions = false;
+
   @ViewChild(AgmMap) public agmMap: AgmMap;
 
   title: string = 'Ubicar Escuelas';
 
   ngAfterViewInit() {
     //this.agmMap.triggerResize();
+  }
+
+
+  public origin = { lat: 20.684697, lng: -103.395303 }
+  public destination = { lat: 20.684697, lng: -103.477303 }
+
+  public getDirection(){
+    this.directions = !this.directions;
   }
 }
