1016daff0bbfd81c3c0fec8fd14b4b04306c060b
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-11-19 23:42:24 -0600

    Solved problems x3

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("src/index.html"));
+app.use(express.static("/src/index.html"));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join('src/index.html'));
+res.sendFile(path.join('/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
